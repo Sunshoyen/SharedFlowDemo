4bd02eba8dd83f950e23a32b90d1380894a9a39b
reira <danilova1267@icloud.com>
2025-11-20 19:32:23 +0700

    sharedFlowInit написание функции

--- app/src/main/java/com/example/sharedflowdemo/DemoViewModel.kt
+++ app/src/main/java/com/example/sharedflowdemo/DemoViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.sharedflowdemo
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 class DemoViewModel : ViewModel() {
     private val _sharedFlow = MutableSharedFlow<Int>()
@@ -11,6 +14,12 @@ class DemoViewModel : ViewModel() {
         sharedFlowInit()
     }
     fun sharedFlowInit() {
+        viewModelScope.launch {
+            for (i in 1..1000) {
+                delay(2000)
+                _sharedFlow.emit(i)
+            }
+        }
     }
 
 }
