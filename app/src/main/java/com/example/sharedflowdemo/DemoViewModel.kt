9471fd24e9ebc92502cf65773fafffb1ef002e3c
reira <danilova1267@icloud.com>
2025-11-20 19:31:04 +0700

    DemoViewModel добавление SharedFlow

--- app/src/main/java/com/example/sharedflowdemo/DemoViewModel.kt
+++ app/src/main/java/com/example/sharedflowdemo/DemoViewModel.kt
@@ -1,6 +1,16 @@
 package com.example.sharedflowdemo
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 
 class DemoViewModel : ViewModel() {
+    private val _sharedFlow = MutableSharedFlow<Int>()
+    val sharedFlow = _sharedFlow.asSharedFlow()
+    init {
+        sharedFlowInit()
+    }
+    fun sharedFlowInit() {
+    }
+
 }
